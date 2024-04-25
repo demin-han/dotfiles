7f521d6e739fa2b93e7327e98b80fe5b7cbcd635
demin.han <demin.han@starfivetech.com>
2024-04-25 17:13:06 +0800

    disable .debug folder which used by perf

--- scripts/install.zsh
+++ scripts/install.zsh
@@ -54,4 +54,8 @@ if [[ ! -d $HOME/.config/nvim ]]; then
   git clone https://github.com/LazyVim/starter $HOME/.config/nvim
   rm -rf $HOME/.config/nvim/.git
 fi
+
+# disable $HOME/.debug folder
+perf config buildid.dir=/dev/null
+
 echo "End install"
