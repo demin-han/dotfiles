9a4d6659c3caaa970ea22051f8d857a6ae03e1c5
demin.han <demin.han@starfivetech.com>
2024-04-25 11:16:59 +0800

    add nvim config

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.relativenumber = false -- Relative line numbers
+vim.opt.tabstop = 4
+vim.g.autoformat = false
+vim.lsp.set_log_level("off")
+vim.o.undofile = false
+vim.o.swapfile = false
+vim.o.wrap = true
